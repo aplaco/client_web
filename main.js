5902371b1822a9d7c6dbfa72d56f952fc05c0930
aplaco <lyon108@naver.com>
2025-04-24 13:11:39 +0900

    동적으로 태그 생성하는 다른방법

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ async function getUsers(pageNum = 0) {
 
   section.innerHTML = tags;
 }
+
+/*
+async function getUsers(pageNum = 0) {
+  const res = await fetch(`http://localhost:8080/api/admin?page=${pageNum}`);
+  const data = await res.json();
+  const userArr = data.users;
+  console.log(userArr);
+
+  const tags = userArr
+    .map(
+      (user) => `
+      <article>
+        <h2>${user.uname}</h2>
+        <p>${user.email}</p>
+        <span>${user.colors}</span>
+      </article>
+    `
+    )
+    .join(""); //tags 반복 끝
+
+  section.innerHTML = tags;
+}
+*/
