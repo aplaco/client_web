af410666689201af77db8bbb4c4c2effa891edd5
aplaco <lyon108@naver.com>
2025-04-24 14:37:38 +0900

    post방식으로 DB에 데이터 저장

--- form.js
+++ form.js
@@ -1,12 +1,31 @@
 //회원정보 DB 저장 함수
 
-const form = document.querySelector("#join");
-const uname = document.querySelector("input[name='uname']").value;
-const email = document.querySelector("input[name='email']").value;
-const colors = document.querySelector("input[name='colors']").value;
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("test");
-  console.log(uname, email, colors);
-});
+document.querySelector("#join").addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendForm();
+  });
+  
+  async function sendForm() {
+    const uname = document.querySelector("input[name='uname']");
+    const email = document.querySelector("input[name='email']");
+    const colors = document.querySelector("input[name='colors']");
+  
+    const userInfo = {
+      uname: uname.value,
+      email: email.value,
+      colors: colors.value,
+    };
+  
+    const strJson = JSON.stringify(userInfo);
+    const data = await fetch("http://127.0.0.1:8080/api/join/create", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: strJson,
+    });
+  
+    const result = await data.json();
+    alert(result.message);
+    location.href = "index.html";
+  }
