dd4d76e6257330fef9b09828292d31815bd856a5
aplaco <lyon108@naver.com>
2025-04-25 09:25:58 +0900

    로그인 페이지 추가

--- form.js
+++ form.js
@@ -1,29 +1,22 @@
-//회원정보 DB 저장 함수
+document.querySelector("#join").addEventListener("submit", sendForm);
 
-document.querySelector("#join").addEventListener("submit", (e) => {
-    e.preventDefault();
-    sendForm();
+async function sendForm(e) {
+  e.preventDefault();
+
+  //아래 두줄의 코드는 form안쪽의 모든 값들을 직렬화 해서 객체로 변환할때 자주 쓰이는 패턴
+  const formData = new FormData(e.target);
+  const formJSON = Object.fromEntries(formData.entries());
+  //formData.entries(); --->  [ [key,value], [key,value], [key,value] ]
+  //Object.fromEntries([ [key,value], [key,value], [key,value] ]) ---> {key: value, key:value, key: value}
+  console.log(formJSON);
+
+  const data = await fetch("http://127.0.0.1:8080/api/join/create", {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify(formJSON),
   });
-  
-  async function sendForm() {
-    const uname = document.querySelector("input[name='uname']");
-    const email = document.querySelector("input[name='email']");
-    const colors = document.querySelector("input[name='colors']");
-  
-    const userInfo = {
-      uname: uname.value,
-      email: email.value,
-      colors: colors.value,
-    };
-  
-    const strJson = JSON.stringify(userInfo);
-    const data = await fetch("http://127.0.0.1:8080/api/join/create", {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: strJson,
-    });
-  
-    const result = await data.json();
-    alert(result.message);
-    location.href = "index.html";
-  }
+
+  //const result = await data.json();
+  //alert(result.message);
+  //location.href = "index.html";
+}
